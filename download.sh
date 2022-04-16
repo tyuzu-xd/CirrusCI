a70cecd51b8c6febe4b61eeb0a6bd1946a1c8fb2
tyuzu-xd <sahruldarmian212@gmail.com>
2022-04-17 02:46:21 +0700

    Ginkgo

--- download.sh
+++ download.sh
@@ -2,10 +2,7 @@
 
 echo "Downloading few Dependecies . . ."
 # Kernel Sources
-git clone --depth 1 https://github.com/NFS-projects/kernel_xiaomi_rosy -b twelve $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
+git clone --depth 1 https://github.com/tyuzu-xd/kernel_xiaomi_ginkgo.git -b elainaX-11 $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 # Toolchain
-git clone --depth 1 https://github.com/NFS-projects/gcc-arm -b 12.x $CIRRUS_WORKING_DIR/GCC32
-git clone --depth 1 https://github.com/NFS-projects/gcc-arm64 -b 12.x $CIRRUS_WORKING_DIR/GCC64
-git clone --depth 1 https://github.com/NFS-projects/gcc-arm -b 11.x $CIRRUS_WORKING_DIR/GCC-11-32
-git clone --depth 1 https://github.com/NFS-projects/gcc-arm64 -b 11.x $CIRRUS_WORKING_DIR/GCC-11-64
-git clone --depth 1 https://gitlab.com/AnGgIt86/nfs-clang.git -b main $CIRRUS_WORKING_DIR/CLANG
+git clone --depth 1 https://github.com/kdrag0n/proton-clang.git $CIRRUS_WORKING_DIR/CLANG
+git clone --depth 1 https://github.com/tyuzu-xd/AnyKernel3.git -b Ginkgo $CIRRUS_WORKING_DIR/AnyKernel
