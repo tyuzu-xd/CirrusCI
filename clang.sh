9c0f0b9b421b2f8d268680dbe0bba07d2e92f8e3
tyuzu-xd <sahruldarmian212@gmail.com>
2022-04-18 02:02:38 +0700

    Ginkgo

--- clang.sh
+++ clang.sh
@@ -40,7 +40,7 @@ compile(){
 cd ${KERNEL_ROOTDIR}
 tg_post_msg "<b>Buiild Kernel Clang started..</b>"
 make -j$(nproc --all) O=out ARCH=arm64 SUBARCH=arm64 ${DEFCONFIG}
-make -j$(nproc --all) ARCH=arm64 SUBARCH=arm64 O=out LD_LIBRARY_PATH="${LLD_VER}/lib:${LD_LIBRARY_PATH} \
+make -j$(nproc --all) ARCH=arm64 SUBARCH=arm64 O=out \
 	       CC=${CLANG_ROOTDIR}/bin/clang \
 	       AR=${CLANG_ROOTDIR}/bin/llvm-ar \
 	       AS=${CLANG_ROOTDIR}/bin/llvm-as \
