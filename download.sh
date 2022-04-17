26f18dc6409a1bb89e1a5208b0d246289f95afb9
tyuzu-xd <sahruldarmian212@gmail.com>
2022-04-18 03:53:04 +0700

    Ginkgo

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@
 
 echo "Downloading few Dependecies . . ."
 # Kernel Sources
-git clone --depth=1 https://github.com/kutemeikito/android_kernel_xiaomi_ginkgo.git -b MiuiR-WIP $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
+git clone --depth=1 https://github.com/tyuzu-xd/kernel_xiaomi_ginkgo.git -b elainaX-11 $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 # Toolchain
 #git clone --depth 1 https://github.com/NFS-projects/gcc-arm -b 11.x $CIRRUS_WORKING_DIR/GCC-11-32
 #git clone --depth 1 https://github.com/NFS-projects/gcc-arm64 -b 11.x $CIRRUS_WORKING_DIR/GCC-11-64
