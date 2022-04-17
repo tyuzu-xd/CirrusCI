7b833bbd7bc6ca2945a41904b0a74229f84a433d
tyuzu-xd <sahruldarmian212@gmail.com>
2022-04-18 03:01:51 +0700

    Ginkgo

--- download.sh
+++ download.sh
@@ -4,7 +4,7 @@ echo "Downloading few Dependecies . . ."
 # Kernel Sources
 git clone --depth=1 https://github.com/tyuzu-xd/kernel_xiaomi_ginkgo.git -b elainaX-11 $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 # Toolchain
-#git clone --depth=1 https://github.com/fajar3109/arm-linux-androideabi-4.9 -b main $CIRRUS_WORKING_DIR/GCC-11-32
-#git clone --depth=1 https://github.com/fajar3109/aarch64-linux-android-4.9 -b main $CIRRUS_WORKING_DIR/GCC-11-64
-git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang.git $CIRRUS_WORKING_DIR/CLANG
+git clone --depth=1 https://github.com/mvaisakh/gcc-arm.git -b gcc-new $CIRRUS_WORKING_DIR/GCC-11-32
+git clone --depth=1 https://github.com/mvaisakh/gcc-arm64.git -b gcc-new $CIRRUS_WORKING_DIR/GCC-11-64
+# git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang.git $CIRRUS_WORKING_DIR/CLANG
 git clone --depth=1 https://github.com/tyuzu-xd/AnyKernel3.git -b ginkgo $CIRRUS_WORKING_DIR/AnyKernel
