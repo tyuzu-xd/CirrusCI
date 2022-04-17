97c7af3f750176058be5138472485a60b1e0de7c
tyuzu-xd <sahruldarmian212@gmail.com>
2022-04-18 03:49:45 +0700

    Ginkgo

--- download.sh
+++ download.sh
@@ -6,5 +6,5 @@ git clone --depth=1 https://github.com/kutemeikito/android_kernel_xiaomi_ginkgo.
 # Toolchain
 #git clone --depth 1 https://github.com/NFS-projects/gcc-arm -b 11.x $CIRRUS_WORKING_DIR/GCC-11-32
 #git clone --depth 1 https://github.com/NFS-projects/gcc-arm64 -b 11.x $CIRRUS_WORKING_DIR/GCC-11-64
-git clone --depth=1 https://github.com/xyz-prjkt/xRageTC-clang.git -main $CIRRUS_WORKING_DIR/CLANG
+git clone --depth=1 https://github.com/xyz-prjkt/xRageTC-clang.git -b main $CIRRUS_WORKING_DIR/CLANG
 git clone --depth=1 https://github.com/tyuzu-xd/AnyKernel3.git -b ginkgo $CIRRUS_WORKING_DIR/AnyKernel
