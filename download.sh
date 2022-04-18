b95a719a63d663f5c4b915482d4a51fd9940e1cf
tyuzu-xd <sahruldarmian212@gmail.com>
2022-04-18 13:01:23 +0700

    Ginkgo

--- download.sh
+++ download.sh
@@ -6,5 +6,5 @@ git clone --depth=1 https://github.com/tyuzu-xd/kernel_xiaomi_ginkgo.git -b elai
 # Toolchain
 git clone --depth 1 https://github.com/ImSpiDy/gcc-9.3.0.git -b gcc-32 $CIRRUS_WORKING_DIR/GCC-11-32
 git clone --depth 1 https://github.com/ImSpiDy/gcc-9.3.0.git -b gcc-64 $CIRRUS_WORKING_DIR/GCC-11-64
-git clone --depth=1 https://gitlab.com/ElectroPerf/atom-x-clang.git -b main $CIRRUS_WORKING_DIR/CLANG
+git clone --depth=1 https://gitlab.com/ElectroPerf/atom-x-clang.git -b atom-15 $CIRRUS_WORKING_DIR/CLANG
 git clone --depth=1 https://github.com/tyuzu-xd/AnyKernel3.git -b ginkgo $CIRRUS_WORKING_DIR/AnyKernel
