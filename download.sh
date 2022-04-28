5b68e94ac8b4d101344a163e85c65bf254b6479d
tzuyu-xd <sahruldarmian212@gmail.com>
2022-04-28 22:00:42 +0700

    .

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@
 
 echo "Downloading few Dependecies . . ."
 # Kernel Sources
-git clone --depth=1 https://github.com/tzuyu-xd/kernel_xiaomi_ginkgo.git -b elainaX-11 $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
+git clone --depth=1 https://github.com/tzuyu-xd/android_kernel_xiaomi_ginkgo.git -b MiuiR-WIP $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 # Toolchain
 #git clone --depth 1 https://github.com/sohamxda7/llvm-stable.git -b gcc32 $CIRRUS_WORKING_DIR/GCC-11-32
 #git clone --depth 1 https://github.com/sohamxda7/llvm-stable.git -b gcc64 $CIRRUS_WORKING_DIR/GCC-11-64
