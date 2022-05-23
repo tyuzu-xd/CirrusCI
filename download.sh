5ae678df988c9c812a7430503f33b6388eed9e28
Tzuyu-XD <102547027+tzuyu-xd@users.noreply.github.com>
2022-05-23 21:35:13 +0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Downloading few Dependecies . . ."
 # Kernel Sources
-git clone --depth=1 https://github.com/tzuyu-xd/android_kernel_xiaomi_ginkgo.git -b MiuiR-WIP $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
-# Toolchain
-#git clone --depth 1 https://github.com/sohamxda7/llvm-stable.git -b gcc32 $CIRRUS_WORKING_DIR/GCC-11-32
-#git clone --depth 1 https://github.com/sohamxda7/llvm-stable.git -b gcc64 $CIRRUS_WORKING_DIR/GCC-11-64
-git clone --depth=1 https://github.com/kdrag0n/proton-clang.git $CIRRUS_WORKING_DIR/CLANG
-git clone --depth=1 https://github.com/tyuzu-xd/AnyKernel3.git -b ginkgo $CIRRUS_WORKING_DIR/AnyKernel
+git config --global user.email "sahruldarmian212@gmail.com" -y && git config --global user.name "tzuyu-xd"
+git clone --depth=1 https://github.com/ryujin-xd/android_kernel_xiaomi_ginkgo-1 -b Q $CIRRUS_WORKING_DIR/KERNEL
+git clone --depth=1 https://github.com/kdrag0n/proton-clang $CIRRUS_WORKING_DIR/CLANG
